3a888ada04db6179bb508386bb246c4d315ce3a4
SergSha <96518320+SergSha@users.noreply.github.com>
2022-05-28 22:28:52 +0300

    Update nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -7,20 +7,11 @@ yum -y install nfs-utils
 # Enable and start firewalld
 
 systemctl start firewalld
-systemctl enable firewalld -f
-
-# Add firewall rules
-
-#firewall-cmd --add-service="nfs3" \
-#--add-service="rpc-bind" \
-#--add-service="mountd" \
-#--permanent
-
-#firewall-cmd --reload
+systemctl enable firewalld
 
 # Add line to /etc/fstab
 
-echo "192.168.50.10:/srv/share/       /mnt/   nfs     vers=3,proto=udp,noauto,x-systemd.automount     0       0" >> /etc/fstab
+echo "192.168.50.10:/srv/share/ /mnt/ nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
 
 # Reload daemons
 
